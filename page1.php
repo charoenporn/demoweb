52d141e152e46ff6cc332e7a181605fe7cf0d479
charoenporn <tmon368@gmail.com>
2020-04-13 11:06:57 +0700

    new file

---
+++ page1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first page PHP script!";
+$x = 15985;
+var_dump(is_int($x));
+
+$x = 59.85;
+var_dump(is_int($x));
+?>
+
+</body>
+</html>
+</html>
---
+++ page2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My second page PHP script!";
+
+$x = 5985;
+var_dump(is_numeric($x));
+
+$x = "5985";
+var_dump(is_numeric($x));
+
+$x = "59.85" + 100;
+var_dump(is_numeric($x));
+
+$x = "Hello";
+var_dump(is_numeric($x));
+?>
+
+</body>
+</html>
+</html>
